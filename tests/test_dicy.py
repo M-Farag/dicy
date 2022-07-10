4cafa25dbc2c91c7067cf4754124ede916905fdc
M-Farag <mina.farag@icloud.com>
2022-07-10 02:26:13 +0200

    ADD Dice object functionality

--- tests/test_dicy.py
+++ tests/test_dicy.py
@@ -4,21 +4,23 @@
 
 import pytest
 
-
-from dicy import dicy
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+from dicy import dicy as d
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+class TestDicyObject(object):
+
+    @pytest.fixture
+    def dice_object(self, tmpdir):
+        x = d.Dicy()
+        yield x
+
+    """Good group"""
+
+    def test_init_dice_object_will_init_a_list_of_six_objects(self, dice_object):
+        assert len(dice_object) == 6
+
+    def test_can_iterate_on_the_dice_faces(self, dice_object):
+        x = 1
+        for d in dice_object:
+            assert (d.face == x)
+            x += 1
